9024e6628243f4add2a6fbd4e9fe65ed992b63ae
pankajkumarchauhan371 <pankajkumarchauhanp371@gmail.com>
2026-08-31 13:19:31 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 };
 
-export const API_BASE_URL = 'https://mern-task-manager-app-qtie.vercel.app';
+export const API_BASE_URL = 'http://localhost:4800';
