10f87bf20bf8690067d604833b6ac2ffe3ed2ef8
pankajkumarchauhan371 <pankajkumarchauhanp371@gmail.com>
2026-09-02 11:29:41 +0530

    Update utils.js

--- frontend/src/utils.js
+++ frontend/src/utils.js
@@ -4,4 +4,4 @@ export const notify = (message, type) => {
     toast[type](message);
 };
 
-export const API_BASE_URL = 'https://task-manager-xi-nine-32.vercel.app';
+export const API_BASE_URL = 'http://localhost:4800';
